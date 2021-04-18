c49905a35dd8fa5a0469dd0598a75c3d72dde6f5
Kent Chadwick <kento.mc@gmail.com>
2021-04-18 09:58:30 +0100

    Added environment variables to wordpress config in docker-compose

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -23,13 +23,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-// define( 'DB_NAME', 'wordpress');
+define( 'DB_NAME', 'wordpress');
 
-// /** MySQL database username */
-// define( 'DB_USER', 'wordpress');
+/** MySQL database username */
+define( 'DB_USER', 'wordpress');
 
-// /** MySQL database password */
-// define( 'DB_PASSWORD', 'wordpress');
+/** MySQL database password */
+define( 'DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
 define( 'DB_HOST', 'db:3306');
